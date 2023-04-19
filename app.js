ddf7bf248ee631ccbb3cb2ad8e70b880a0757880
Al-Fareed <alfareedss472@gmail.com>
2023-04-19 21:14:26 +0530

    Handling errors for Unsupported routes
    If the url is incorrect then it displays custom error messages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const HttpError = require('./models/http-errors');
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
@@ -8,6 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/places',placesRoutes); //middleware from places-routes
+
+app.use((req,res,next)=>{
+    const error = new HttpError('Could not find the place',404);
+    throw  error;
+});
 
 app.use((error,req,res,next)=>{
     if(res.headerSent){
