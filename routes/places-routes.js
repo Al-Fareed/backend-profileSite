8a2a439fa14ae30a34ba67d6b3c187bf2b3436d9
Al-Fareed <alfareedss472@gmail.com>
2023-04-19 14:33:50 +0530

    adding routes based on places and user id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    console.log('GET request in Places');
-    res.json({message:'Working bro!'});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous place you know",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th street, New York 1001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const user = DUMMY_PLACES.find((u) => {
+    return u.creator === userId;
+  });
+  res.json({ user });
 });
 
 module.exports = router;
