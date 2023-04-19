0ba9ad29efd918723783deb64532c095596de317
Al-Fareed <alfareedss472@gmail.com>
2023-04-19 16:21:47 +0530

    adding controllers
    added controllers to handle place and user to get by id

---
+++ controller/places-controller.js
@@ -0,0 +1,57 @@
+const HttpError = require("../models/http-errors");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous place you know",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th street, New York 1001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    //if found places with id then:
+    // return res.status(404).json({ message: "Could not find for " + placeId }); //!we can also write this to handle error
+    //TODO: handling the error in more efficient way, I am sending the error to app.js
+    //   if not found places with id then:
+    throw new HttpError(
+      `Could not find a place for the provided id : ${placeId}`,
+      404
+    );
+  }
+  res.json({ place });
+};
+//   function getPlaceById(parameters){} //?we can also define in this manner
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const user = DUMMY_PLACES.find((u) => {
+    return u.creator === userId;
+  });
+  if (!user) {
+    //if found user with id then:
+    // return res.status(404).json({ message: "Could not find place with user id " + userId }); //!Instead of throwing error from here
+    return next(
+      //   if not found user with id then:
+      new HttpError(
+        `Could not find a place for the provided user id : ${userId}`,
+        404
+      )
+    );
+  }
+  res.json({ user });
+};
+
+// To export more than one function 
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
