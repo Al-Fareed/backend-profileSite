628bd7b82cc0c569aade723f4cb04a0ebd5388cf
Al-Fareed <alfareedss472@gmail.com>
2023-04-21 15:22:58 +0530

    user Signup
    creating user and adding it to database

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -1,6 +1,7 @@
 const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-errors");
+const User = require("../models/user");
 const DUMMY_USERS = [
   {
     id: "u1",
@@ -14,37 +15,46 @@ const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   // to validate the user inputs through validationResult, which is in user-routes.js
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid credentials entered", 422);
+    return next( new HttpError("Invalid credentials entered", 422));
   }
-  const { name, email, password } = req.body;
+  const { name, email, password,places } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (error) {
+    return next(new HttpError("Could not Sign Up", 500));
+  }
 
-  // checks if user already exists
-  const userExist = DUMMY_USERS.find((u) => u.email === email);
-  if (userExist) {
-    throw new HttpError("User already exists, try with other email id", 422);
+  if (existingUser) {
+    return next(new HttpError("User already exist,Please login", 422));
   }
-// creates a new user
-  const createdUser = {
-    id: uuid(),
+  // creates a new user
+  const createdUser = new User({
     name, //similar to name :name
     email,
+    image: "https://picsum.photos/200/300.webp",
     password,
-  };
-  DUMMY_USERS.push(createdUser);
-  res.status(201).json({ user: createdUser });
+    places
+  });
+  try {
+    await createdUser.save();
+  } catch (error) {
+    return next(new HttpError("Signup failed, try again", 500));
+  }
+  res.status(201).json({ user: createdUser.toObject({getters : true}) });
 };
 
 // For login
 const login = (req, res, next) => {
   const { email, password } = req.body;
-//   Check whether the user exists 
+  //   Check whether the user exists
   const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
   if (!identifiedUser || !identifiedUser.password === password) {
-    throw new HttpError("User does not exist", 401);
+    return next(new HttpError("User does not exist", 401));
   }
   res.json({ message: "Logged in Successfully" });
 };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,21 +2,25 @@
 // * Here we have only middleware and the functions are from places-controller.js
 
 const express = require("express");
-const HttpError = require('../models/http-errors');
-const {check} = require('express-validator');
-const usersControllers = require('../controller/users-controller');
+// const HttpError = require('../models/http-errors');
+const { check } = require("express-validator");
+const usersControllers = require("../controller/users-controller");
 
 const router = express.Router();
 
-// 
+//
 router.get("/", usersControllers.getUsers);
 // for new user
-router.get('/signup',[
-    check('name').not().isEmpty(),
-    check('email').normalizeEmail().isEmail(), //checks the email is valid or not
-    check('password').isLength({min:6})
-],usersControllers.signup);
-// to login 
-router.post('/login',usersControllers.login);
+router.post(
+  '/signup',
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(), //checks the email is valid or not
+    check("password").isLength({ min: 6 }),
+  ],
+  usersControllers.signup
+);
+// to login
+router.post("/login", usersControllers.login);
 
 module.exports = router;
