34c0e59893a4a8eb73238e2b04739eb623f0fd62
Al-Fareed <alfareedss472@gmail.com>
2023-04-21 11:17:17 +0530

    establishing connection between express and mongoose

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const HttpError = require('./models/http-errors');
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/user-routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const HttpError = require("./models/http-errors");
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/user-routes");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places',placesRoutes); //middleware from places-routes
-app.use('/api/users',usersRoutes); //middleware from users-routes
+app.use("/api/places", placesRoutes); //middleware from places-routes
+app.use("/api/users", usersRoutes); //middleware from users-routes
 
-app.use((req,res,next)=>{
-    const error = new HttpError('Could not find the place',404);
-    throw  error;
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find the place", 404);
+  throw error;
 });
 
-app.use((error,req,res,next)=>{
-    if(res.headerSent){
-        return next(error);
-    }
-    res.status(error.code || 500); //indicates something went wrong on the server
-    res.json({message:error.message || 'An unknown error occurred!'});
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500); //indicates something went wrong on the server
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-
-
-app.listen(5000);
+mongoose.connect('mongodb+srv://AlFareed:Oyl1MxjZiH5FY96l@cluster0.urwkxxh.mongodb.net/places?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('Connected to database ');
+    
+    app.listen(5000);
+})
+.catch(()=>{
+    console.log('Error in connecting');
+    
+});
